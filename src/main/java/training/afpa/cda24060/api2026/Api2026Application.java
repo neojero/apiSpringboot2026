0a83d5e4d48ff09c87f1340564e73efb1b05bff7
neojero <jboebion@gmail.com>
2026-02-05 14:06:37 +0100

    github actions : .env

--- src/main/java/training/afpa/cda24060/api2026/Api2026Application.java
+++ src/main/java/training/afpa/cda24060/api2026/Api2026Application.java
@@ -9,15 +9,23 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class Api2026Application {
 
     static {
-        // Charge le .env dès que la classe est chargée (avant le contexte Spring)
-        Dotenv dotenv = Dotenv.configure()
-                .directory("./")
-                .load();
-        // Injecte les variables dans les propriétés système
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
+        String dbUrl = System.getenv("DB_URL");
+        if (dbUrl == null) {
+            // 2. Si non, charge le .env (développement local)
+            Dotenv dotenv = Dotenv.configure()
+                    .directory("./")
+                    .load();
+            System.setProperty("DB_URL", dotenv.get("DB_URL"));
+            System.setProperty("DB_USER", dotenv.get("DB_USER"));
+            System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+            System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
+        } else {
+            // 3. Sinon, utilise les variables d'environnement (GitHub Actions)
+            System.setProperty("DB_URL", dbUrl);
+            System.setProperty("DB_USER", System.getenv("DB_USER"));
+            System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
+            System.setProperty("DB_DRIVER", System.getenv("DB_DRIVER"));
+        }
     }
 
     public static void main(String[] args) {
