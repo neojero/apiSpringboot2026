9289befe0325ea3cd0dec085e6c8f0fb53d8c6f4
neojero <jboebion@gmail.com>
2026-02-05 16:21:38 +0100

    github actions : .env

--- src/main/java/training/afpa/cda24060/api2026/Api2026Application.java
+++ src/main/java/training/afpa/cda24060/api2026/Api2026Application.java
@@ -9,25 +9,17 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class Api2026Application {
 
     static {
-        // 1. Vérifie si on est en environnement CI (GitHub Actions)
-        String isCi = System.getenv("CI");
-        if ("true".equals(isCi)) {
-            // En CI, injecte directement les variables d'environnement dans les propriétés système
-            System.setProperty("spring.datasource.url", System.getenv("DB_URL"));
-            System.setProperty("spring.datasource.username", System.getenv("DB_USER"));
-            System.setProperty("spring.datasource.password", System.getenv("DB_PASSWORD"));
-            System.setProperty("spring.datasource.driver-class-name", System.getenv("DB_DRIVER"));
-        } else {
-            // En local, charge le .env
-            Dotenv dotenv = Dotenv.configure()
-                    .directory("./")
-                    .load();
-            System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
-            System.setProperty("spring.datasource.username", dotenv.get("DB_USER"));
-            System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
-            System.setProperty("spring.datasource.driver-class-name", dotenv.get("DB_DRIVER"));
-        }
+        // Charge le .env dès que la classe est chargée (avant le contexte Spring)
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .load();
+        // Injecte les variables dans les propriétés système
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
     }
+
 
     public static void main(String[] args) {
         SpringApplication.run(Api2026Application.class, args);
