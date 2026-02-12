658a8b8504608ce5a507e0b9aa8a85c14ecf97a3
neojero <jboebion@gmail.com>
2026-02-12 09:58:40 +0100

    github actions : suppression env

--- src/main/java/training/afpa/cda24060/api2026/Api2026Application.java
+++ src/main/java/training/afpa/cda24060/api2026/Api2026Application.java
@@ -1,9 +1,7 @@
 package training.afpa.cda24060.api2026;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Api2026Application {
@@ -11,5 +9,4 @@ public class Api2026Application {
     public static void main(String[] args) {
         SpringApplication.run(Api2026Application.class, args);
     }
-
 }
