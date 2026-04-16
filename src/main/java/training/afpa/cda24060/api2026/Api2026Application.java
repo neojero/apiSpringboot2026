b42646fa7ca2ba4470c34311072aa69c226ae0e9
neojero <jboebion@gmail.com>
2026-04-16 14:14:50 +0200

    test github actions

--- src/main/java/training/afpa/cda24060/api2026/Api2026Application.java
+++ src/main/java/training/afpa/cda24060/api2026/Api2026Application.java
@@ -8,5 +8,8 @@ public class Api2026Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Api2026Application.class, args);
+
+        // commentaires test
+        
     }
 }
